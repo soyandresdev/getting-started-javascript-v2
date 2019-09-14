b20f7c477ab7bdca1788c7011646d64193dda30b
Andres Hernandez Lozano <soyandresdev@gmail.com>
2019-09-14 02:05:27 -0600

    [ADD] Operations in Javascript

---
+++ 3-operations.js
@@ -0,0 +1,15 @@
+3 + 4 // 7
+43 - 1 //42
+43 / 2 // 21.5
+43 * 1 // 86
+40 %  2 // 0
+41 %  2 // 1
+"Andres" + "Hernandez" // Andres Hernandez
+!false // true
+3.0 == 3 // true
+3 < 4 // true
+3 > 2 // true
+3 <= 3 // true
+
+true || false // true
+true && false // false
