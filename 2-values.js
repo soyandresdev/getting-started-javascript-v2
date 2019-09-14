fd157655b15caa8a8ca9527530d265c4e79e4d1e
Andres Hernandez Lozano <soyandresdev@gmail.com>
2019-09-14 02:05:05 -0600

    [ADD] Values in Javascript

---
+++ 2-values.js
@@ -0,0 +1,9 @@
+var number = 42
+var float = 3.14
+var string = 'Hello, Friend.'
+var trueBoolean = true
+var falseBoolean = false
+var nullValue = null
+// undefined
+var arrayList = [1, 2, 3]
+var objectDiccionario = {name: 'Andres Hernandez'}
